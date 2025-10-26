1ed3a0d74a0ad6334772c9c1fc91934d888fbdbe
Mohd Owais <ansariowais1230@gmail.com>
2025-10-26 19:48:42 +0530

    update script.js

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-async function getWeather() {
-    const location = document.getElementById("locationInput").value;
-    const apiKey = "08cdef1b5d2043afbc5150506252309";
-    const url =  https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY
- 
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
-  
-      if (data.error) {
-        document.getElementById("weatherResult").innerText = "Location not found.";
-      } else {
-        const temp = data.current.temp_c;
-        const condition = data.current.condition.text;
-        document.getElementById("weatherResult").innerText =
-          `🌍 Location: ${data.location.name}, ${data.location.country}\n🌡️ Temperature: ${temp}°C\n🌤️ Condition: ${condition}`;
-      }
-    } catch (error) {
-      document.getElementById("weatherResult").innerText = "Error fetching weather data.";
-    }
-  }
+ async function getWeather() {
+ const location = document.getElementById("locationInput").value;
+ const apiKey = "08cdef1b5d2043afbc5150506252309";
+ const url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${location}&aqi=yes`;
+
+try {
+ const response = await fetch(url);
+const data = await response.json();
+
+ if (data.error) {
+document.getElementById("weatherResult").innerText = "Location not found.";
+ } else {
+ const temp = data.current.temp_c;
+const condition = data.current.condition.text;
+ document.getElementById("weatherResult").innerText =`🌍 Location: ${data.location.name}, ${data.location.country}\n🌡️ Temperature: ${temp}°C\n🌤️ Condition: ${condition}`;
+ }
+ } catch (error) {
+ document.getElementById("weatherResult").innerText = "Error fetching weather data.";
+ }
+}
+
+
 
   
+
